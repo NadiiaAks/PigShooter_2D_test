c2d6f5daacc25f950777681fb94f696a960290ca
Nadiia Aksonova <nadizarubina11@gmail.com>
2022-09-08 23:45:29 +0300

    Add make arrow object

---
+++ Assets/Scripts/PigBihavior.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PigBihavior : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+    [SerializeField] GameObject arrow;
+    [SerializeField] Transform arrowSpot;
+    [SerializeField] Transform arrowRotation;
+    TargetMoving target;
+
+    private bool isBullet;
+
+    private void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Target").GetComponent<TargetMoving>();
+    }
+
+
+    private void Update()
+    {
+        if (target.IsTimeToAttack)
+        {
+            Debug.Log("Attack work");
+            animator.SetTrigger("attack");
+        }
+
+        MakeArrow();
+    }
+
+    private void MakeArrow()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            Instantiate(arrow, arrowSpot.position, arrowRotation.rotation);
+        }
+    }
+
+}
