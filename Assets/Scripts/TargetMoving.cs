9ad38f9806d306bc3c95453eaaba786c801a5b3c
Nadiia Aksonova <nadizarubina11@gmail.com>
2022-09-08 15:13:50 +0300

    Add animation

--- Assets/Scripts/TargetMoving.cs
+++ Assets/Scripts/TargetMoving.cs
@@ -6,14 +6,36 @@ public class TargetMoving : MonoBehaviour
 {
     [SerializeField] float speed;
     [SerializeField] Vector2 newPosition;
+    [SerializeField] Animator animator;
+
+    private bool isTimeToAttack;
 
     void Update()
     {
         Moving();
+        AttackAnimation();
+    }
+
+    public bool IsTimeToAttack
+    {
+        get => isTimeToAttack;
     }
 
     private void Moving()
     {
         transform.position = Vector2.MoveTowards(transform.position, newPosition, speed * Time.deltaTime);
+        if((Vector2)transform.position == newPosition)
+        {
+            isTimeToAttack = true;
+            Debug.Log("Time to attack");
+        }
+    }
+
+    public void AttackAnimation()
+    {
+        if(isTimeToAttack)
+        {
+            animator.SetTrigger("attack");
+        }
     }
 }
